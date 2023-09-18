f42ad056948fdf1adf2e34f273af7a3e6c03990b
JosueMoora <josuedavidmora99@gmail.com>
2023-09-18 18:36:21 -0400

    [Fix] server

--- build/server.js
+++ build/server.js
@@ -6,7 +6,6 @@ var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 const restaurants_1 = __importDefault(require("./routes/restaurants"));
 const users_1 = __importDefault(require("./routes/users"));
 const favorites_1 = __importDefault(require("./routes/favorites"));
@@ -15,20 +14,21 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'X-Api-Version'],
-    credentials: true,
-    origin: [
-        'http://localhost:3000',
-        'https://restaurants-app-drab.vercel.app',
-        'https://restaurants-app-josuemoora.vercel.app',
-        'https://restaurants-app-git-main-josuemoora.vercel.app'
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
-}));
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+// Lectura y parseo del body
+app.use(express_1.default.json({ limit: '1024mb' }));
+// It parses incoming requests with URL-encoded payloads
+app.use(express_1.default.urlencoded({ limit: '1024mb', extended: true }));
+// Public dir
+app.use(express_1.default.static('public'));
 const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 8080;
 app.use((0, morgan_1.default)('dev'));
-app.use(express_1.default.json());
 app.use('/api/restaurants', restaurants_1.default);
 app.use('/api', users_1.default);
 app.use('/api/favorites', favorites_1.default);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv'
 import express from 'express'
-import cors from 'cors'
 import restaurantsRouter from './routes/restaurants'
 import usersRouter from './routes/users'
 import favoritesRouter from './routes/favorites'
@@ -9,24 +8,24 @@ import cookieParser from 'cookie-parser'
 dotenv.config()
 const app = express()
 app.use(cookieParser())
-app.use(
-  cors({
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-CSRF-Token', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'X-Api-Version'],
-    credentials: true,
-    origin: [
-      'http://localhost:3000',
-      'https://restaurants-app-drab.vercel.app',
-      'https://restaurants-app-josuemoora.vercel.app',
-      'https://restaurants-app-git-main-josuemoora.vercel.app'
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
-  })
-)
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
+// Lectura y parseo del body
+app.use(express.json({ limit: '1024mb' }))
 
+// It parses incoming requests with URL-encoded payloads
+app.use(express.urlencoded({ limit: '1024mb', extended: true }))
+
+// Public dir
+app.use(express.static('public'))
 const PORT = process.env.PORT ?? 8080
 
 app.use(morgan('dev'))
-app.use(express.json())
 
 app.use('/api/restaurants', restaurantsRouter)
 app.use('/api', usersRouter)
