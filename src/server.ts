9ec32d029b040f27d5d612a210632807e58d80b6
JosueMoora <josuedavidmora99@gmail.com>
2023-09-18 15:13:49 -0400

    [Fix] CORS

--- src/server.ts
+++ src/server.ts
@@ -9,21 +9,25 @@ import cookieParser from 'cookie-parser'
 dotenv.config()
 const app = express()
 app.use(cookieParser())
-app.use(cors({
-  allowedHeaders: [
-    'Origin',
-    'X-Requested-With',
-    'Content-Type',
-    'Accept'
-  ],
-  credentials: true,
-  origin: [
-    'http://localhost:3000',
-    'https://tailorhub-2b8f3ad8d-josuemoora.vercel.app/',
-    'https://tailorhub-back-osw7-dev.fl0.io/'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-}))
+app.use(
+  cors({
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+    credentials: true,
+    origin: [
+      'http://localhost:3000',
+      'https://tailorhub-2b8f3ad8d-josuemoora.vercel.app/',
+      'https://tailorhub-dev.fl0.io/'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+  })
+)
+app.use(express.json({ limit: '1024mb' }))
+
+// It parses incoming requests with URL-encoded payloads
+app.use(express.urlencoded({ limit: '1024mb', extended: true }))
+
+// Public dir
+app.use(express.static('public'))
 const PORT = process.env.PORT ?? 8080
 
 app.use(morgan('dev'))
