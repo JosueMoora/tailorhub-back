31de7d7d6b9128a88aae25047e7a376cb79a0f78
JosueMoora <josuedavidmora99@gmail.com>
2023-09-18 15:39:50 -0400

    [Fix] tsc

--- src/server.ts
+++ src/server.ts
@@ -21,13 +21,7 @@ app.use(
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   })
 )
-app.use(express.json({ limit: '1024mb' }))
 
-// It parses incoming requests with URL-encoded payloads
-app.use(express.urlencoded({ limit: '1024mb', extended: true }))
-
-// Public dir
-app.use(express.static('public'))
 const PORT = process.env.PORT ?? 8080
 
 app.use(morgan('dev'))
