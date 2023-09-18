9b72d1a9322f0699276fca67f1b59745952c7241
JosueMoora <josuedavidmora99@gmail.com>
2023-09-18 19:48:14 -0400

    [FIX] jwt

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express'
-import { generateJWT } from '../../helpers/jwt/generateJWT'
+// import { generateJWT } from '../../helpers/jwt/generateJWT'
 import { readUsersFile } from '../../helpers/users/getUsers'
 import { User } from '../../models/Users'
 import bcrypt from 'bcrypt'
-
+import jwt from 'jsonwebtoken'
 export function authenticateUser (req: Request, res: Response): Response {
   const { username, password } = req.body
   const users = readUsersFile()
@@ -21,7 +21,7 @@ export function authenticateUser (req: Request, res: Response): Response {
     })
   }
 
-  const token = generateJWT(user.id)
+  const token = jwt.sign({ id: user.id }, process.env.TOKEN_SECRET ?? 'tokentest', { expiresIn: '4h' })
   res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'none', maxAge: 1000 * 60 * 60 })
   return res.status(200).json('acceso exitoso')
 }
