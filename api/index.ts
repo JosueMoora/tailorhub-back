c90d7741b4b5b20fe970993e433610f4cc3251ef
Josue Mora <103001095+JosueMoora@users.noreply.github.com>
2024-05-01 12:49:39 -0400

    Fix index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
-import restaurantsRouter from '../src/routes/restaurants'
-import usersRouter from '../src/routes/users'
-import favoritesRouter from '../src/routes/favorites'
+import restaurantsRouter from './routes/restaurants'
+import usersRouter from './routes/users'
+import favoritesRouter from './routes/favorites'
 import morgan from 'morgan'
 import cookieParser from 'cookie-parser'
 dotenv.config()
@@ -19,21 +19,15 @@ app.use(cors({
   credentials: true,
   origin: [
     'http://localhost:3000',
-    'https://restaurants-app-josuemoora.vercel.app'
+    'https://restaurants-app-josuemoora.vercel.app',
+    'https://restaurants-app-drab.vercel.app/login'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }))
-// Lectura y parseo del body
-app.use(express.json({ limit: '1024mb' }))
-
-// It parses incoming requests with URL-encoded payloads
-app.use(express.urlencoded({ limit: '1024mb', extended: true }))
-
-// Public dir
-app.use(express.static('public'))
-const PORT = process.env.PORT ?? 8080
+const PORT = process.env.PORT ?? 3001
 
 app.use(morgan('dev'))
+app.use(express.json())
 
 app.use('/api/restaurants', restaurantsRouter)
 app.use('/api', usersRouter)
