64204db4c6bd61ce9163a9475fa0b1b81ebc0a02
JosueMoora <josuedavidmora99@gmail.com>
2024-05-01 12:57:38 -0400

    [Fix] login error

--- api/index.ts
+++ api/index.ts
@@ -19,21 +19,15 @@ app.use(cors({
   credentials: true,
   origin: [
     'http://localhost:3000',
-    'https://restaurants-app-josuemoora.vercel.app'
+    'https://restaurants-app-josuemoora.vercel.app',
+    'https://restaurants-app-drab.vercel.app/login'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }))
-// Lectura y parseo del body
-app.use(express.json({ limit: '1024mb' }))
-
-// It parses incoming requests with URL-encoded payloads
-app.use(express.urlencoded({ limit: '1024mb', extended: true }))
-
-// Public dir
-app.use(express.static('public'))
-const PORT = process.env.PORT ?? 8080
+const PORT = process.env.PORT ?? 3001
 
 app.use(morgan('dev'))
+app.use(express.json())
 
 app.use('/api/restaurants', restaurantsRouter)
 app.use('/api', usersRouter)
